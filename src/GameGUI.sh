492333635e88c76d6cbd9df637b1c406ac60e4a2
Joan T. Matamalas Llodrà <jtmatamalas@gmail.com>
2012-10-29 16:38:04 +0100

    Bug fixed: now the shell script invoke the agents central agent and the coordinator agent

--- src/GameGUI.sh
+++ src/GameGUI.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp "./bin:./lib:./lib/Base64.jar:./lib/iiop.jar:./lib/jade.jar:./lib/jadeTools.jar:./lib/commons-codec-1.3.jar"  -Xmx400m -Xms400m jade.Boot -nomtp -gui central:sma.CentralAgent;coord:sma.CoordinatorAgent
+java -cp "./src:./bin:./lib:./lib/Base64.jar:./lib/iiop.jar:./lib/jade.jar:./lib/jadeTools.jar:./lib/commons-codec-1.3.jar"  -Xmx400m -Xms400m jade.Boot -nomtp -gui -agents "central:sma.CentralAgent;coord:sma.CoordinatorAgent"
